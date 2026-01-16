b52d0a29234a53c7ab1882bd55d963f0416d3899
Jerannn <jerandeduro16@gmail.com>
2026-01-16 16:27:29 +0800

    Add vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
-
     tailwindcss(),
   ],
+  base: process.env.VITE_BASE_PATH || "/blog-app",
 });
