9d6a0b1e73811fe6280e6d772304e7c2f5966bdf
Jerannn <jerandeduro16@gmail.com>
2026-01-16 16:13:28 +0800

    add .env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://rmyzfhngqjjkxwcoebfk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJteXpmaG5ncWpqa3h3Y29lYmZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDI1MjIsImV4cCI6MjA4MzY3ODUyMn0.yUOHJDz0QYTefuSBgijFkVlPiPdZjUmocBVtaK1_S1U";
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
